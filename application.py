9ce1afef33d6492b9707b8637592a058d601f6e2
HashirGJ8842 <hameedhashirniazi@gmail.com>
2020-01-20 19:57:32 +0530

    Added login route, imported flash, url_for, redirect

--- application.py
+++ application.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, flash
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'enter-the-worst-password'
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == "Test" and form.password.data == "test":
+            flash(f'Welcome {form.username.data}', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Username or Password is incorrect', 'danger')
+    return render_template('login.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
